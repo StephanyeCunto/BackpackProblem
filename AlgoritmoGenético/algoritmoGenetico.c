abc78910549e52704dedf1e2df4f0c15307f6ee8
StephanyeCunto <stephanyecristine6@gmail.com>
2025-02-18 20:29:04 -0300

    Algoritmo genético funcionando

--- AlgoritmoGenético/algoritmoGenetico.c
+++ AlgoritmoGenético/algoritmoGenetico.c
@@ -5,27 +5,30 @@
 #include <float.h>
 
 #define NUM_ITENS 26
-#define TAM_POPULACAO 100  
-#define NUM_GERACOES 100 
-#define TAXA_MUTACAO 0.15  
-#define TAXA_ELITISMO 0.1  
+#define TAM_POPULACAO 100
+#define NUM_GERACOES 100
+#define TAXA_MUTACAO 0.15
+#define TAXA_ELITISMO 0.1
 #define CAPACIDADE 10
-#define TAMANHO_TORNEIO 3  
+#define TAMANHO_TORNEIO 3
 
-typedef struct {
+typedef struct
+{
     int peso;
     int valor;
     char nome[20];
 } Item;
 
-typedef struct {
+typedef struct
+{
     int *cromossomo;
     int fitness;
     int peso_total;
-    float razao_fitness;  
+    float razao_fitness;
 } Individuo;
 
-typedef struct {
+typedef struct
+{
     Individuo *individuos;
     int tamanho;
     int melhor_fitness;
@@ -34,60 +37,66 @@ typedef struct {
 
 Item itens[NUM_ITENS];
 
-void inicializar_razoes_valor_peso(void);
-Individuo* criar_individuo(void);
-void destruir_individuo(Individuo *ind);
-Populacao* criar_populacao(int tamanho);
+Populacao *criar_populacao(int tamanho);
 void destruir_populacao(Populacao *pop);
 int calcular_fitness(Individuo *ind);
 void inicializar_populacao(Populacao *pop);
-Individuo* selecao_torneio(Populacao *pop);
+Individuo *selecao_torneio(Populacao *pop);
 void crossover_uniforme(Individuo *pai1, Individuo *pai2, Individuo *filho1, Individuo *filho2);
 void mutacao(Individuo *ind);
 void atualizar_estatisticas_populacao(Populacao *pop);
 void preservar_elitismo(Populacao *antiga, Populacao *nova);
 void imprimir_estatisticas(int geracao, Populacao *pop);
 void imprimir_solucao(Individuo *ind);
-
-void inicializar_itens(void) {
-    for (int i = 0; i < NUM_ITENS; i++) {
+void algoritmo_genetico(void);
+void imprimir_cabecario(void);
+void imprimir_rodape(void);
+void inicializar_itens(void)
+{
+    for (int i = 0; i < NUM_ITENS; i++)
+    {
+        sprintf(itens[i].nome, "Item %d", i + 1);
         itens[i].peso = rand() % 10 + 1;
-        itens[i].valor = rand() % 20 + 1;        
+        itens[i].valor = rand() % 20 + 1;
     }
 }
 
-void imprimir_itens(void) {
-    for (int i = 0; i < NUM_ITENS; i++) {
-        printf("Nome: %s, Peso: %d, Valor: %d, Razao (Valor/Peso): %.2f\n", 
-                itens[i].nome, itens[i].peso, itens[i].valor);
+void imprimir_itens(void)
+{
+    printf("+-----------+--------+--------+\n");
+    printf("| Item      | Peso   | Valor  |\n");
+    printf("+-----------+--------+--------+\n");
+    for (int i = 0; i < NUM_ITENS; i++)
+    {
+        printf("| %-9s | %6d | %6d |\n",
+               itens[i].nome, itens[i].peso, itens[i].valor);
     }
+    printf("+-----------+--------+--------+\n");
 }
 
-void destruir_individuo(Individuo *ind) {
-    if (ind) {
-        free(ind->cromossomo);
-        free(ind);
-    }
-}
+Populacao *criar_populacao(int tamanho)
+{
+    Populacao *pop = (Populacao *)malloc(sizeof(Populacao));
 
-Populacao* criar_populacao(int tamanho) {
-    Populacao *pop = (Populacao*)malloc(sizeof(Populacao));
-   
-    pop->individuos = (Individuo*)malloc(tamanho * sizeof(Individuo));
-    
+    pop->individuos = (Individuo *)malloc(tamanho * sizeof(Individuo));
+
     pop->tamanho = tamanho;
     pop->melhor_fitness = 0;
     pop->fitness_medio = 0;
-    
-    for (int i = 0; i < tamanho; i++) {
-        pop->individuos[i].cromossomo = (int*)malloc(NUM_ITENS * sizeof(int));
+
+    for (int i = 0; i < tamanho; i++)
+    {
+        pop->individuos[i].cromossomo = (int *)malloc(NUM_ITENS * sizeof(int));
     }
     return pop;
 }
 
-void destruir_populacao(Populacao *pop) {
-    if (pop) {
-        for (int i = 0; i < pop->tamanho; i++) {
+void destruir_populacao(Populacao *pop)
+{
+    if (pop)
+    {
+        for (int i = 0; i < pop->tamanho; i++)
+        {
             free(pop->individuos[i].cromossomo);
         }
         free(pop->individuos);
@@ -95,178 +104,245 @@ void destruir_populacao(Populacao *pop) {
     }
 }
 
-int calcular_fitness(Individuo *ind) {
+int calcular_fitness(Individuo *ind)
+{
     int peso_total = 0, valor_total = 0;
-    
-    for (int i = 0; i < NUM_ITENS; i++) {
-        if (ind->cromossomo[i]) {
+
+    for (int i = 0; i < NUM_ITENS; i++)
+    {
+        if (ind->cromossomo[i])
+        {
             peso_total += itens[i].peso;
             valor_total += itens[i].valor;
         }
     }
-    
+
     ind->peso_total = peso_total;
-    
-    if (peso_total > CAPACIDADE) {
+
+    if (peso_total > CAPACIDADE)
+    {
         return 0;
     }
-    
+
     return valor_total;
+}
+
+void criar_individuo(Individuo *individuo) {
+    int peso_atual = 0;
+    for (int j = 0; j < NUM_ITENS; j++) {
+        if (peso_atual + itens[j].peso < CAPACIDADE) {
+            individuo->cromossomo[j] = rand() % 2;
+            peso_atual += itens[j].peso;
+        } else {
+            individuo->cromossomo[j] = 0;
+        }
+    }
+    individuo->fitness = calcular_fitness(individuo);
 }
 
 void inicializar_populacao(Populacao *pop) {
     for (int i = 0; i < pop->tamanho; i++) {
-        for (int j = 0; j < NUM_ITENS; j++) {
-            pop->individuos[i].cromossomo[j] = rand() % 2;
-        }
-        pop->individuos[i].fitness = calcular_fitness(&pop->individuos[i]);
+        criar_individuo(&pop->individuos[i]);
     }
     atualizar_estatisticas_populacao(pop);
 }
 
-Individuo* selecao_torneio(Populacao *pop) {
+Individuo *selecao_torneio(Populacao *pop)
+{
     int melhor_idx = rand() % pop->tamanho;
     int melhor_fitness = pop->individuos[melhor_idx].fitness;
-    
-    for (int i = 1; i < TAMANHO_TORNEIO; i++) {
+
+    for (int i = 1; i < TAMANHO_TORNEIO; i++)
+    {
         int idx = rand() % pop->tamanho;
-        if (pop->individuos[idx].fitness > melhor_fitness) {
+        if (pop->individuos[idx].fitness > melhor_fitness)
+        {
             melhor_idx = idx;
             melhor_fitness = pop->individuos[idx].fitness;
         }
     }
-    
+
     return &pop->individuos[melhor_idx];
 }
 
-void crossover_uniforme(Individuo *pai1, Individuo *pai2, Individuo *filho1, Individuo *filho2) {
-    for (int i = 0; i < NUM_ITENS; i++) {
-        if (rand() % 2) {
+void crossover_uniforme(Individuo *pai1, Individuo *pai2, Individuo *filho1, Individuo *filho2)
+{
+    for (int i = 0; i < NUM_ITENS; i++)
+    {
+        if (rand() % 2)
+        {
             filho1->cromossomo[i] = pai1->cromossomo[i];
             filho2->cromossomo[i] = pai2->cromossomo[i];
-        } else {
+        }
+        else
+        {
             filho1->cromossomo[i] = pai2->cromossomo[i];
             filho2->cromossomo[i] = pai1->cromossomo[i];
         }
     }
-    
+
     filho1->fitness = calcular_fitness(filho1);
     filho2->fitness = calcular_fitness(filho2);
 }
 
-void mutacao(Individuo *ind) {
-    for (int i = 0; i < NUM_ITENS; i++) {
-        if ((float)rand() / RAND_MAX < TAXA_MUTACAO) {
+void mutacao(Individuo *ind)
+{
+    for (int i = 0; i < NUM_ITENS; i++)
+    {
+        if ((float)rand() / RAND_MAX < TAXA_MUTACAO)
+        {
             ind->cromossomo[i] = !ind->cromossomo[i];
         }
     }
     ind->fitness = calcular_fitness(ind);
 }
 
-void atualizar_estatisticas_populacao(Populacao *pop) {
+void atualizar_estatisticas_populacao(Populacao *pop)
+{
     int soma_fitness = 0;
     pop->melhor_fitness = 0;
-    
-    for (int i = 0; i < pop->tamanho; i++) {
+
+    for (int i = 0; i < pop->tamanho; i++)
+    {
         soma_fitness += pop->individuos[i].fitness;
-        if (pop->individuos[i].fitness > pop->melhor_fitness) {
+        if (pop->individuos[i].fitness > pop->melhor_fitness)
+        {
             pop->melhor_fitness = pop->individuos[i].fitness;
         }
     }
-    
+
     pop->fitness_medio = (float)soma_fitness / pop->tamanho;
 }
 
-void preservar_elitismo(Populacao *antiga, Populacao *nova) {
+void preservar_elitismo(Populacao *antiga, Populacao *nova)
+{
     int num_elite = (int)(TAM_POPULACAO * TAXA_ELITISMO);
-    
-    for (int i = 0; i < antiga->tamanho - 1; i++) {
-        for (int j = 0; j < antiga->tamanho - i - 1; j++) {
-            if (antiga->individuos[j].fitness < antiga->individuos[j + 1].fitness) {
+
+    for (int i = 0; i < antiga->tamanho - 1; i++)
+    {
+        for (int j = 0; j < antiga->tamanho - i - 1; j++)
+        {
+            if (antiga->individuos[j].fitness < antiga->individuos[j + 1].fitness)
+            {
                 Individuo temp = antiga->individuos[j];
                 antiga->individuos[j] = antiga->individuos[j + 1];
                 antiga->individuos[j + 1] = temp;
             }
         }
     }
-    
-    for (int i = 0; i < num_elite; i++) {
+
+    for (int i = 0; i < num_elite; i++)
+    {
         memcpy(nova->individuos[i].cromossomo, antiga->individuos[i].cromossomo, NUM_ITENS * sizeof(int));
         nova->individuos[i].fitness = antiga->individuos[i].fitness;
     }
 }
 
-void imprimir_estatisticas(int geracao, Populacao *pop) {
+void imprimir_estatisticas(int geracao, Populacao *pop)
+{
     printf("Geracao %d: Melhor Fitness = %d, Fitness Medio = %.2f\n",
            geracao, pop->melhor_fitness, pop->fitness_medio);
 }
 
-void imprimir_solucao(Individuo *ind) {
+void imprimir_solucao(Individuo *ind)
+{
     printf("\nMelhor solucao encontrada:\n");
     printf("Cromossomo: ");
-    for (int i = 0; i < NUM_ITENS; i++) {
+    for (int i = 0; i < NUM_ITENS; i++)
+    {
         printf("%d ", ind->cromossomo[i]);
     }
     printf("\nFitness: %d\n", ind->fitness);
     printf("Peso Total: %d\n", ind->peso_total);
     printf("\nItens selecionados:\n");
-    for (int i = 0; i < NUM_ITENS; i++) {
-        if (ind->cromossomo[i]) {
-            printf("- %s (Peso: %d, Valor: %d)\n", 
+    printf("+-----------+--------+--------+\n");
+    printf("| Item      | Peso   | Valor  |\n");
+    printf("+-----------+--------+--------+\n");
+    for (int i = 0; i < NUM_ITENS; i++)
+    {
+        if (ind->cromossomo[i])
+        {
+            printf("| %-9s | %6d | %6d |\n",
                    itens[i].nome, itens[i].peso, itens[i].valor);
         }
     }
+    printf("+-----------+--------+--------+\n");
 }
 
-void algoritmo_genetico(void) {
-    
+void algoritmo_genetico(void)
+{
+
     Populacao *pop_atual = criar_populacao(TAM_POPULACAO);
     Populacao *pop_nova = criar_populacao(TAM_POPULACAO);
-    
+
     inicializar_populacao(pop_atual);
-    
-    for (int gen = 0; gen < NUM_GERACOES; gen++) {
+
+    for (int gen = 0; gen < NUM_GERACOES; gen++)
+    {
         preservar_elitismo(pop_atual, pop_nova);
         int num_elite = (int)(TAM_POPULACAO * TAXA_ELITISMO);
-        for (int i = num_elite; i < TAM_POPULACAO - 1; i += 2) {
+        for (int i = num_elite; i < TAM_POPULACAO - 1; i += 2)
+        {
             Individuo *pai1 = selecao_torneio(pop_atual);
             Individuo *pai2 = selecao_torneio(pop_atual);
-            
-            crossover_uniforme(pai1, pai2, &pop_nova->individuos[i], 
-                             &pop_nova->individuos[i + 1]);
-            
+
+            crossover_uniforme(pai1, pai2, &pop_nova->individuos[i],
+                               &pop_nova->individuos[i + 1]);
+
             mutacao(&pop_nova->individuos[i]);
             mutacao(&pop_nova->individuos[i + 1]);
         }
-        
+
         atualizar_estatisticas_populacao(pop_nova);
-        
-        if (gen % 10 == 0) {
-            imprimir_estatisticas(gen, pop_nova);
+
+
+        if (gen % 10 == 0)
+        {
+            printf("| %-8d | %14d | %14.2f |\n",
+                   gen, pop_nova->melhor_fitness, pop_nova->fitness_medio);
         }
-        
+           
         Populacao *temp = pop_atual;
         pop_atual = pop_nova;
         pop_nova = temp;
     }
-    
+
+    imprimir_rodape();
+
     Individuo *melhor = &pop_atual->individuos[0];
-    for (int i = 1; i < TAM_POPULACAO; i++) {
-        if (pop_atual->individuos[i].fitness > melhor->fitness) {
+    for (int i = 1; i < TAM_POPULACAO; i++)
+    {
+        if (pop_atual->individuos[i].fitness > melhor->fitness)
+        {
             melhor = &pop_atual->individuos[i];
         }
     }
-    
+
     imprimir_solucao(melhor);
-    
+
     destruir_populacao(pop_atual);
     destruir_populacao(pop_nova);
 }
 
-int main(void) {
+void imprimir_cabecario(void)
+{
+    printf("+---------------------------------------------+\n");
+    printf("| Geracao  | Melhor Fitness | Fitness Medio  |\n");
+    printf("+---------------------------------------------+\n");
+}
+
+void imprimir_rodape(void)
+{
+    printf("+---------------------------------------------+\n");
+}
+
+int main(void)
+{
     srand((unsigned)time(NULL));
-    inicializar_itens(); 
+    inicializar_itens();
     imprimir_itens();
+    imprimir_cabecario();
     algoritmo_genetico();
+
     return 0;
 }
